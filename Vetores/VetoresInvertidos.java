9d4c6579f4a31c0d4166631bacc2ede688d16ce8
fabiotnagamine <84332616+fabiotnagamine@users.noreply.github.com>
2021-06-11 18:32:24 -0300

    Update VetoresInvertidos.java

--- Vetores/VetoresInvertidos.java
+++ Vetores/VetoresInvertidos.java
@@ -1,1 +1,28 @@
+public class VetoresTrocados {
+
+	public static void main(String[] args) {
+		int posicao = 20;
+	
+		int vetor[] = new int[posicao];
+		//Atribuindo valores para o vetor
+		for (int i = 0; i < posicao; i++) {
+			vetor[i] = i * 5;
+			System.out.println("vetor na posição " + i + ": " + vetor[i]);
+		}
+		int inversor;
+		for(int i = 0; i < 20; i++) {
+			for(int j = 0; j < i; j++) {
+				inversor = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = inversor;
+			}
+			System.out.println("***********Resultado*************");
+			for(int j = 0; j < 20; j++) {
+				System.out.println("Vetor na posição " + j + ": " + vetor[j]);
+			}
+		}
+		
+	}
+
+}
 
