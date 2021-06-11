122cd02df7b679675ec4b16de9a34e6ab037bb6a
fabiotnagamine <84332616+fabiotnagamine@users.noreply.github.com>
2021-06-11 18:33:06 -0300

    Create Vetores.java

---
+++ Vetores/Vetores.java
@@ -0,0 +1,20 @@
+public class Vetor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int vetor1[] = new int[10];
+		int vetor2[] = new int[10];
+		for (int i = 0; i <10; i++) {
+			vetor1[i] = i;
+		}
+		for (int j = 0; j < 10; j++ ) {
+			vetor2[j] = vetor1[j];
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Vetor1 ["+ i +"]: " + vetor1[i]);
+			System.out.println("Vetor2:["+ i +"]: " + vetor2[i]);
+		}
+	}
+
+}
