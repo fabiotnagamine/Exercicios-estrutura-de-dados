407a70de75df812b90f78bf4adad3b5dcadda970
fabiotnagamine <84332616+fabiotnagamine@users.noreply.github.com>
2021-09-02 19:15:25 -0300

    Create No.java

---
+++ Lista de exercicios 2/No.java
@@ -0,0 +1,26 @@
+public class No {
+    Dado d;
+    No proximo;
+    
+    public No(char d_char){
+        Dado f = new Dado(d_char);
+        this.d = f;
+    }
+    public void setProximo(No prox){
+        this.proximo = prox;
+    }
+    public No getProximo(){
+        return this.proximo;        
+    }
+        (){
+            if 
+        }
+
+    public String toString() {
+        if (proximo != null){
+            return this.hashCode() + " - " + d.toString()  + " Prox-> " + proximo.hashCode();
+            else 
+                return this.hashCode() + " - " + d.toString() + "Proximo não existe";
+        }
+    }
+}
